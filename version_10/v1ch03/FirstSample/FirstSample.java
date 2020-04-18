6bf5efe09e2aa5c4c5a3fb5a6e616606b86f0cff
Nick <nick.yangbin.ma@gmail.com>
2020-04-18 17:44:19 +0800

    3.4 变量 - 3.5 运算符.

--- version_10/v1ch03/FirstSample/FirstSample.java
+++ version_10/v1ch03/FirstSample/FirstSample.java
@@ -24,10 +24,34 @@ public class FirstSample  // 源代码文件名必须和公共类的名字相同
     *     Double.NEGATIVE_INFINITY
     *     x == Double.NaN  // is never true
     *     Double.isNaN(x)
+    *
+    *     char:
+    *     \\是因为Java字符串必须转义,其实我只想输入一个单独的反斜线
+    *     'A' \\u0000 - \\uffff
+    *     '\\uxxxx'代表一个可以打印的char, \\uxxxx会在解析代码前处理
+    *     \\uxxxx代表直接的一个字符: public static void main(String\\u005B\\u005D args)
+    *     最好不要使用char
+    *
+    *     boolean:
+    *     Java 0 和 boolean 不能自动转换
+    *
+    *     常量:
+    *     习惯全大写,只能被赋值一次,赋值后不能再更改
+    *     final double PI = 3.14;
+    *
+    * 枚举
+    *    Enum must not be local
+    *    enum Size {S, M, L};
+    *    Size s = Size.M;
+    *
     */
+   enum Size {S, M, L};
+
    public static void main(String[] args)
    {
       System.out.println("We will not use 'Hello, World!'");
       System.out.println("0X1.1p-3 = " + 0X11.1p-3 + " = " + (16 + 1 + 1.0 / 16) / 8);
+      Size s = Size.M;
+      System.out.println("Size.M = " + s);
    }
 }
